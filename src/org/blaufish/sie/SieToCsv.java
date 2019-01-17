72659bfd59e70206eaac3ecfa5fd5e696ad82376
blaufish <blaufish@users.noreply.github.com>
2019-01-17 14:31:27 +0100

    displayCsv2 - earnings matrix; year as rows, months as columns

--- src/org/blaufish/sie/SieToCsv.java
+++ src/org/blaufish/sie/SieToCsv.java
@@ -2,6 +2,7 @@ package org.blaufish.sie;
 
 import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class SieToCsv {
 
@@ -16,6 +17,8 @@ public class SieToCsv {
 		for (String arg : args)
 			parser.parseSei(arg);
 		displayCsv();
+		System.out.println();
+		displayCsv2();
 	}
 
 	private static TreeMap<Integer, Double> financialResult() {
@@ -72,5 +75,26 @@ public class SieToCsv {
 			System.out.printf("%.0f;", amount);
 		}
 		System.out.println();
+	}
+
+	private static void displayCsv2() {
+		TreeMap<Integer, Double> earnings = financialEarnings();
+		TreeSet<Integer> years = new TreeSet<>();
+		for (int yearmonth : earnings.keySet())
+			years.add(yearmonth / 100);
+		System.out.print(";");
+		for (int i = 1; i < 13; i++)
+			System.out.printf("%d;", i);
+		System.out.println();
+		for (int year : years) {
+			System.out.printf("%d;", year);
+			for (int i = 1; i < 13; i++) {
+				Double amount = earnings.get(year * 100 + i);
+				if (amount == null)
+					amount = 0.0d;
+				System.out.printf("%.0f;", amount);
+			}
+			System.out.println();
+		}
 	}
 }
